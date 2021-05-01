85329c55cbffcd439009f2d1d34a1ed47c505191
OBrien-reece <indeche.evans.o@gmail.com>
2021-05-01 18:27:26 +0300

    intial commit

---
+++ node/app.js
@@ -0,0 +1,4 @@
+const validator = require('validator');
+// const getEmail = validator.isEmail('indeche.evans.o@gmail.com');
+// console.log(validator.isEmail('indeche.evans.o@gmail.com'));
+console.log(validator.isURL('https://www.npmjs.com/package/validator'));
